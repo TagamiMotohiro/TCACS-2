781fb8d9769f5c30d8344278647bd2412951aeb8
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2022-05-19 15:42:06 +0900

    0519授業分

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//ハンズオン02
+//int n = 2;
+//if (n % 2 == 1)
+//{
+//    Console.WriteLine("奇数");
+//}
+//else
+//{
+//    Console.WriteLine("偶数");
+//}
+//ハンズオン03
+//string Answer = "z";
+//string userAnswer;
+//while (true)
+//{
+//    Console.WriteLine("入力");
+//    userAnswer = Console.ReadLine() ?? "";
+//    if (userAnswer == Answer)
+//    {
+//        Console.WriteLine("正解");
+//        break;
+//    }
+//    else
+//    {
+//        Console.WriteLine("不正解");
+//    }
+//}
+//ハンズオン04
+//int n = 1;
+//int sum = 0;
+//while (n <= 100)
+//{
+//    sum += n;
+//    n++;
+//}
+//Console.WriteLine(sum);
