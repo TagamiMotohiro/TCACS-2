907df9d61b896abd1d2bf119ba4667d12d0d9463
TagamiMotohiro <105412324+TagamiMotohiro@users.noreply.github.com>
2022-06-16 14:05:09 +0900

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//ハンズオン02
+
+//ハンズオン02(偶数奇数判定)
 //int n = 2;
 //if (n % 2 == 1)
 //{
@@ -10,7 +11,7 @@
 //{
 //    Console.WriteLine("偶数");
 //}
-//ハンズオン03
+//ハンズオン03(文字当て)
 //string Answer = "z";
 //string userAnswer;
 //while (true)
@@ -27,7 +28,7 @@
 //        Console.WriteLine("不正解");
 //    }
 //}
-//ハンズオン04
+//ハンズオン04(1～100の数字)
 //int n = 1;
 //int sum = 0;
 //while (n <= 100)
@@ -36,3 +37,30 @@
 //    n++;
 //}
 //Console.WriteLine(sum);
+//ハンズオン05
+//string userAnswer;
+//string answer = "z";
+//bool count = true;
+//do
+//{
+//    if(count==false)
+//    {
+//        Console.WriteLine("不正解");
+//    }
+//    Console.WriteLine("1文字入力");
+//    userAnswer=Console.ReadLine()?? "";
+//    if(count==true)
+//    {
+//        count = false;
+//        continue;
+//    }
+//}
+//while(userAnswer!=answer);
+//Console.WriteLine("正解");
+//ハンズオン06(for分1+2+3+…+100)
+int n=0;
+for (int i = 0;i<=100;i++)
+{
+    n=n+i;
+}
+Console.WriteLine(n);
